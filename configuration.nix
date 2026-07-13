03a0ca42825c20043ab32043a45fe194678ed706
Nick Zou <z.nick00@gmail.com>
2026-07-13 17:19:39 -0400

    add nerd font

--- configuration.nix
+++ configuration.nix
@@ -112,6 +112,10 @@
     zoxide
   ];
 
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
