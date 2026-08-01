4f42fba76500cd9667a6d4430531ccdac39fc105
Nick Zou <z.nick00@gmail.com>
2026-08-01 17:45:54 -0400

    add github cli

--- configuration.nix
+++ configuration.nix
@@ -117,6 +117,7 @@
     lazygit
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     gcc
+    gh
     git
     go
     networkmanagerapplet
